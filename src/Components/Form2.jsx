270b16447376a1010674b7975ffaaddb63bc5558
Lah-deh <successasokere@gmail.com>
2025-10-26 23:02:08 +0100

    updsteee

--- src/Components/Form2.jsx
+++ src/Components/Form2.jsx
@@ -4,7 +4,7 @@ import * as Yup from "yup";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import Toast from "../Components/Toast.jsx";
-import Link from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 const api = axios.create({
   baseURL: "https://mockdata-93rw.onrender.com",
